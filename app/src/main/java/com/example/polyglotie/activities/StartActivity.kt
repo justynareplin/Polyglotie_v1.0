cb31b6ffd31acb0619762ccc074905466abb9cca
justyniaczek <justynareplin@gmail.com>
2021-11-24 17:39:19 +0100

    Added Auto login

--- app/src/main/java/com/example/polyglotie/activities/StartActivity.kt
+++ app/src/main/java/com/example/polyglotie/activities/StartActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.example.polyglotie.R
+import com.example.polyglotie.firebase.FirestoreClass
 
 class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +13,16 @@ class StartActivity : AppCompatActivity() {
         setContentView(R.layout.activity_start)
 
         val startBtn : Button = findViewById<Button>(R.id.startButton)
+        var currentUserID = FirestoreClass().getCurrentUserId()
+
+        if(currentUserID.isNotEmpty()){
+            val intent= Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
         startBtn.setOnClickListener {
+
             val intent= Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
